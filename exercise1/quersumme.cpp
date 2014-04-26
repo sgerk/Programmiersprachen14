dff7cd314458557b4b9c5a0608cc5f5671526ae7
Sergej Gerk <=>
2014-04-26 22:57:30 +0200

    Quersumme

---
+++ exercise1/quersumme.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <unittest++/UnitTest++.h>
+using namespace std;
+
+unsigned int quersumme(unsigned zahl) {
+	int sum = 0;
+	
+	while ( zahl > 0 ) {
+		sum += zahl % 10;
+		zahl /= 10;
+	}
+
+	return sum;
+}
+
+SUITE(describe_quersumme)
+{
+	TEST(quersumme_of_12_should_be_3)
+	{
+		CHECK_EQUAL(3, quersumme(12));
+	}
+	TEST(factorial_of_555_should_be_15)
+	{
+		CHECK_EQUAL(15, quersumme(555));
+	}
+}
+int main()
+{
+	return UnitTest::RunAllTests();
+}
